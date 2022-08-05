f97e376d2a287ac8fa5fde4cafb04bd00849c2fe
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-05 16:29:48 +0200

    Update mediabox.sh

--- mediabox.sh
+++ mediabox.sh
@@ -3,6 +3,7 @@ apt-get update
 apt-get upgrade
 
 echo "Install Radarr..."
+mkdir /opt/radarr/data
 docker pull linuxserver/radarr
 
 docker run -d --name=radarr -e PUID=1000 -e PGID=1000 -e TZ=Europe/London -p 7878:7878 -v /opt/radarr/data:/config -v /media/NAS/movies:/movies -v /media/NAS/download/:/downloads --restart unless-stopped lscr.io/linuxserver/radarr:latest

648bc99232b80daed5f6d7fc5a8c47c6621f2e3e
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-05 15:30:37 +0200

    Update init.sh
    Add portainer start

--- init.sh
+++ init.sh
@@ -9,3 +9,4 @@ usermod -aG docker pi
 
 echo "Install Portainer (Visualizing Docker containers)..."
 docker pull portainer/portainer:linux-arm
+docker run --restart always -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer:linux-arm

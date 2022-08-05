fdf1ede02abad2253bcc141c90c3292cce166af9
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-05 20:32:16 +0200

    Update mediabox.sh

--- mediabox.sh
+++ mediabox.sh
@@ -7,4 +7,4 @@ cd rpi-radarr/
 # Buikd image
 docker build -t rpi-radarr .
 # Run docker image on port 7878
-docker run --restart always -d -p 7878:7878 rpi-radarr
+docker run --restart always -d -p 7878:7878 -v /media/NAS:/media/NAS rpi-radarr

43c74469540ee3538c9120acc48b7f8ba6d3ca6f
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-05 15:54:46 +0200

    Create jeedom.sh

---
+++ jeedom.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+"Install Jeedom..."
+mkdir /opt/jeedom/www
+mkdir /opt/jeedom/db
+docker pull jeedom/jeedom:V4-stable
+docker run --name jeedom-server -v /opt/jeedom/www:/var/www/html -v /opt/jeedom/db:/var/lib/mysql -p 9080:80 –restart always jeedom/jeedom:V4-stable

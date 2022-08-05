ebe3059fb63e6d00cfee3dad162a2fc2de3920c7
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-05 20:27:23 +0200

    Update mediabox.sh

--- mediabox.sh
+++ mediabox.sh
@@ -3,14 +3,3 @@ apt-get update
 apt-get upgrade
 
 echo "Install Radarr..."
-docker pull linuxserver/radarr
-
-docker run -d \
-  --name=radarr \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Europe/London \
-  -p 7878:7878 \
-  -v /opt/radarr/data:/config \
-  --restart unless-stopped \
-  lscr.io/linuxserver/radarr:latest

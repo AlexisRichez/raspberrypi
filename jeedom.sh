2dfef49f14ec274157d644f9c092e86f9d8cb1f0
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-08 18:20:58 +0200

    Update jeedom.sh

--- jeedom.sh
+++ jeedom.sh
@@ -3,4 +3,4 @@ echo "Install Jeedom..."
 mkdir /opt/jeedom
 mkdir /opt/jeedom/mysql
 
-docker run --name jeedom-mysql -v /opt/jeedom/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=jeedom -e MYSQL_PASSWORD=jeedom --detach --publish 3306:3306 --restart always mariadb:10.1
+docker run --name jeedom-mysql -v /opt/jeedom/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=jeedom -e MYSQL_PASSWORD=jeedom --detach --publish 3306:3306 --restart always linuxserver/mariadb

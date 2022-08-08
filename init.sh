0c9f6843889229ea7934b731eaa1733ff24bbc65
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-08 13:42:04 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ echo "Install Docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 usermod -aG docker pi
+systemctl start docker
 
 echo "Install Portainer (Visualizing Docker containers)..."
 docker pull portainer/portainer:linux-arm

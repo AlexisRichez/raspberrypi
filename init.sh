dee153b2c6c34599d126bac8b7e4e566d865960b
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-08 13:29:03 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,9 @@ apt-get update
 apt-get upgrade
 
 echo "Install Docker..."
-apt-get install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+usermod -aG docker pi
 
 echo "Install Portainer (Visualizing Docker containers)..."
 docker pull portainer/portainer:linux-arm

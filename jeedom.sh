f0517ca04b391035b725f2a5d73ba3447fa1b557
AlexisRichez <48288407+AlexisRichez@users.noreply.github.com>
2022-08-08 18:39:57 +0200

    Update jeedom.sh

--- jeedom.sh
+++ jeedom.sh
@@ -9,4 +9,4 @@ USB2=/dev/serial/by-id/usb-0658_0200-if00
 
 docker run --name jeedom-mysql -v /opt/jeedom/mysql:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=jeedom -e MYSQL_PASSWORD=jeedom --detach --publish 3306:3306 --restart always linuxserver/mariadb
 
-docker create --net host --name jeedom-server -v /opt/jeedom/html:/var/www/html -e ROOT_PASSWORD=password -e APACHE_PORT=9080 -e SSH_PORT=9022 -e MODE_HOST=1 --device $RFXCOM:/dev/ttyUSB0 --device $USB2:/dev/ttyACM0 --restart always jeedom/jeedom:master
+docker create --net host --name jeedom-server -v /opt/jeedom/html:/var/www/html -e ROOT_PASSWORD=password -e APACHE_PORT=9080 -e SSH_PORT=9022 -e MODE_HOST=1 --device $RFXCOM:/dev/ttyUSB0 --device $USB2:/dev/ttyACM0 --restart always jeedom/jeedom
